7f1790502829074dd4b0c8e393df573e2aefb969
Adonys Kevin <adonyskevinms@gmail.com>
2021-02-19 22:59:14 -0300

    Adicionando background, botoes e padding

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,48 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "Contador de Pessoas",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      home: Stack(
         children: <Widget>[
-          Text(
-            "Pessoas: 0",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            "Pode Entrar!",
-            style: TextStyle(
-                color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
+          Image.asset("images/restaurant.jpg", fit: BoxFit.cover, height: 1000),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Pessoas: 0",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: FlatButton(
+                        onPressed: () {},
+                        child: Text(
+                          "+1",
+                          style: TextStyle(fontSize: 40, color: Colors.white),
+                        )),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: FlatButton(
+                        onPressed: () {},
+                        child: Text(
+                          "-1",
+                          style: TextStyle(fontSize: 40, color: Colors.white),
+                        )),
+                  )
+                ],
+              ),
+              Text(
+                "Pode Entrar!",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30),
+              )
+            ],
           )
         ],
       )));
